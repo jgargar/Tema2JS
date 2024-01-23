7994ddd2d1a108f3998b789d7607f93d6cfa2cf9
Jesus W11 <jgargar307@iesmartinezm.es>
2024-01-23 23:31:02 +0100

    kcjkgdhoiucvibfud

--- DWEC/Tema3/Ejercicios/ActJsonServer_JesusGarcia/script.js
+++ DWEC/Tema3/Ejercicios/ActJsonServer_JesusGarcia/script.js
@@ -17,5 +17,122 @@
     const heroes = "http://localhost:3000/heroes"
 
     //Tarea 1
+    let btnCargarImagenes = document.createElement("button")
+    btnCargarImagenes.textContent = "Cargar imagenes"
+    btnCargarImagenes.addEventListener("click", () => {
+        fetch(usuarios)
+        .then(response => response.json())
+        .then(data => {
+          data.forEach(user => {
+            const img = document.createElement('img')
+            img.src = user.foto
+            document.body.appendChild(img)
+          })
+        })
+    })
+    document.body.append(btnCargarImagenes)
+
+    //Tarea 2
+    let btnAniadirUsuario = document.createElement("button")
+    btnAniadirUsuario.textContent = "Añadir mi Usario"
+    btnAniadirUsuario.addEventListener("click", () => {
+        //Primero creo el usuario
+        const miUsuario = {
+            id: "4",
+            usuario: 'Jesus',
+            email: 'jdgarciaa11@gmail.com',
+            foto: 'https://media.licdn.com/dms/image/D4D03AQEoAfe-P2HfLg/profile-displayphoto-shrink_400_400/0/1643188498565?e=1711584000&v=beta&t=CK7xJ-_Y6j9OqoqjuoLEmFY8YVFIGc6dFip_wCG3FGk',
+        }
+        //Depues hago un fetch
+        fetch(usuarios,{
+            method: "POST",
+            body : JSON.stringify(miUsuario),
+            headers: {'Content-type': 'application/json'}
+        })
+        .then(function(response){
+            //Rescato la respuesta para ver si se ha insertado correctamente
+            console.log(response.status)
+            if(response.ok){
+                return response.json()
+            }
+        })
+        .then(console.log)
+        .catch(error => console.log(error)) //Capturo los error que pueda ocasionar
+    })
+    document.body.append(btnAniadirUsuario)
+
+    //Tarea 3
+    let btnModificarImagenes = document.createElement("button")
+    btnModificarImagenes.textContent = "Modificar Imagenes"
+    btnModificarImagenes.addEventListener("click", () => {
+        fetch(usuarios)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(user => {
+                fetch(usuarios + "/" + user.id,{
+                    method: 'PATCH',
+                    body : JSON.stringify({ foto : data[0].foto }),
+                    headers: {
+                        'Content-type': 'application/json'
+                    }
+                })
+                .then(function(response){
+                    //Rescato la respuesta para ver si se ha insertado correctamente
+                    console.log(response.status)
+                    if(response.ok){
+                        return response.json()
+                    }
+                })
+                .then(console.log)
+                .catch(error => console.log(error))
+            })
+        })
+    })
+    document.body.append(btnModificarImagenes)
+
+    //Tarea 4
+    let btnModificarNombre = document.createElement("button")
+    btnModificarNombre.textContent = "Modificar mi nombre"
+    btnModificarNombre.addEventListener("click", () => {
+        fetch(usuarios + "/4",{
+            method: 'PATCH',
+            body : JSON.stringify({ nombre : "Jesus Garcia" }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then(function(response){
+            //Rescato la respuesta para ver si se ha insertado correctamente
+            console.log(response.status)
+            if(response.ok){
+                return response.json()
+            }
+        })
+        .then(console.log)
+        .catch(error => console.log(error))
+    })
+    document.body.append(btnModificarNombre)
+
+    //Tarea 5
+    let btnEliminarHeroe = document.createElement("button")
+    btnEliminarHeroe.textContent = "Eliminar heroe"
+    btnEliminarHeroe.addEventListener("click", () => {
+        fetch(heroes + "/2",{
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then(function(response){
+            //Rescato la respuesta para ver si se ha insertado correctamente
+            console.log(response.status)
+            if(response.ok){
+                return response.json()
+            }
+        })
+        .then(console.log)
+        .catch(error => console.log(error))
+    })
+    document.body.append(btnEliminarHeroe)
     
 }
